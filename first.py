42d793a208e637d4630e1575344675cb99c32c59
Edward Miro <c1ph0r@protonmail.com>
2020-06-07 13:04:57 -0700

    Add files via upload

---
+++ first.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+
+#Print string
+print("Hello, World!")
+print('\n') #new line
+print('Hello, World!')
+print(""""This string runs
+multiple lines!""")
+print("this string is "+"awesome")
