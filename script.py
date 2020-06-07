0a8f334ecaedd9dffe88fcc94827d63e170a63f6
Edward Miro <c1ph0r@protonmail.com>
2020-06-07 13:15:14 -0700

    Add files via upload

---
+++ script.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+#Variables and Methods
+
+quote = "He was like a father to me."
+print(quote)
+print(quote.upper()) #uppercase
+print(quote.lower()) #lowercase
+print(quote.title()) #titlecase
+
+print(len(quote)) #length
+
+name = "Edward" #string
+age = 69 #int
+gpa = 4.20 #float
+
+print(int(age))
+print(int(30.1))
+print(int(30.9))
+
+print("My name is " + name + " and I am " + str(age) + " years old.")
+
+age += 1
+print(age)
+
+birthday = 1
+age += birthday
+print(age)
